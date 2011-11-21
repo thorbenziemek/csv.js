5cfda388b81fac10d78cbfc0949e687fae770a7a
Thorben Ziemek <mail@thorben.ziemek.de>
2011-11-21 21:45:47 +0100

    hide private functions for better compression

--- src/Csv.js
+++ src/Csv.js
@@ -1,177 +1,179 @@
 var jscsv = {};
 
-/*
-   file = record *(CRLF record) [CRLF]
-   record = field *(COMMA field)
-   field = (escaped / non-escaped)
-   escaped = DQUOTE *(TEXTDATA / COMMA / CR / LF / 2DQUOTE) DQUOTE
-   non-escaped = *TEXTDATA
-   COMMA = %x2C
-   CR = %x0D ;as per section 6.1 of RFC 2234 [2]
-   DQUOTE =  %x22 ;as per section 6.1 of RFC 2234 [2]
-   LF = %x0A ;as per section 6.1 of RFC 2234 [2]
-   CRLF = CR LF ;as per section 6.1 of RFC 2234 [2]
-   TEXTDATA =  %x20-21 / %x23-2B / %x2D-7E
-*/
+jscsv.Csv = function() {
 
-jscsv.Csv = function(config) {
-	var me = this;
-	
-	me.config = config || {
-		forceWinLinebreaks : false
-	};
-	me.state = {
-		currentSymbol : 'record',
-		lastChar : null
-	};
-	me.endRecord();
-};
+    var
+    STATE_RECORD = 0,
+    STATE_ESCAPED = 1,
+    STATE_NON_ESCAPED = 2,
+    STATE_DQUOTE_IN_ESCAPED = 3,
 
-jscsv.Csv.prototype = {
+    CHAR_CR = 0x0D,
+    CHAR_LF = 10,
+    CHAR_DQUOTE = 0x22,
+    CHAR_COMMA = 0x2C,
 
-	parse : function(string) {
-		var me = this;
-	
-		for (var i=0, len = string.length; i < len; i++) {
-			me.parseNext(string.charCodeAt(i));
-		}
-		
-		me.endRecord();
-	},
-	
-	endRecord : function() {
-		var me = this;
-		
-		if (me.record && me.record.started) {
-			me.endField();
-			
-			if (me.config.onRecord) {
-				me.config.onRecord(me.record.fields);
-			}
-		}
-		
-		me.record = {
-			started : false,
-			field : '',
-			fields : []
-		};
-		
-		me.state.currentSymbol = 'record';
-	},
+    constructor = function(config) {
+        var me = this;
 
-	parseNext : function(char) {
-		var me = this;
-		//console.log(me.state.currentSymbol, char, String.fromCharCode(char));
-		
-		switch (me.state.currentSymbol) {
-			case 'record':
-				me.fromRecord(char); break;
-			case 'escaped':
-				me.fromEscaped(char); break;
-			case 'dquoteInEscaped':
-				me.fromDquoteInEscaped(char); break;
-			case 'nonEscaped':
-				me.fromNonEscaped(char); break;
-		}
-		
-		me.state.lastChar = char;
-	},
-	
-	fromRecord : function(char) {
-		var me = this;
-		
-		switch (char) {
-			case 0x0D: // CR
-				break;
-			case 10: // LF
-				if (!me.forceWinLinebreaks ||
-					 (me.forceWinLinebreaks && me.state.lastChar == 0x0D)
-				   ) {
-					me.endRecord();
-					break;
-				} 
-			case 0x22: // "
-				me.transitionTo('escaped');
-				break;
-			case 0x2C: // ,
-				this.endField();
-				break;
-			default:
-				me.transitionTo('nonEscaped');
-				me.addChar(char);
-		}
-	},
-	
-	endField : function() {
-		var me = this;
-	
-		me.record.fields.push(me.record.field);
-		me.record.field = '';
-		me.state.currentSymbol = 'record';
-	},
-	
-	fromEscaped : function(char) {
-		var me = this;
-	
-		switch (char) {
-			case 0x22:
-				me.transitionTo('dquoteInEscaped');
-				return;
-			default:
-				me.addChar(char);
-		}
-	},
-	
-	transitionTo : function(symbol) {
-		//console.log('transitionTo', symbol);
-		this.state.currentSymbol = symbol;
-	},
-	
-	addChar : function(char) {
-		var me = this;
-		
-		me.record.started = true;
-		me.record.field += String.fromCharCode(char);
-	},
-	
-	fromNonEscaped : function(char) {
-		var me = this;
-		
-		switch (char) {
-			case 0x0D: // CR
-				break;
-			case 10: // LF
-				if (!me.forceWinLinebreaks ||
-					 (me.forceWinLinebreaks && me.state.lastChar == 0x0D)
-				   ) {
-					me.endRecord();
-					break;
-				} 
-			case 0x22: // "
-				throw new Error("Unexpected symbol: \" in unescaped record");
-			case 0x2C: // ,
-				this.endField();
-				break;
-			default:
-				me.addChar(char);
-		}
-	},
-	
-	fromDquoteInEscaped : function(char) {
-		var me = this;
-	
-		switch (char) {
-			case 0x22: // "
-				me.transitionTo('escaped');
-				me.addChar(char);
-				return;
-			default:
-				return me.fromNonEscaped(char);
-		}
-	},
-	
-	clearState : function() {
-		this.state = {};
-	}
+        me.config = config || {
+            forceWinLinebreaks : false
+        };
 
-};
+        me.state = {
+            currentSymbol : STATE_RECORD,
+            lastChar : null
+        };
+
+        endRecord.call(me);
+    },
+
+    parseNext = function(character) {
+        var me = this;
+
+        switch (me.state.currentSymbol) {
+            case STATE_RECORD:
+                fromRecord.call(me, character); break;
+            case STATE_ESCAPED:
+                fromEscaped.call(me, character); break;
+            case STATE_DQUOTE_IN_ESCAPED:
+                fromDquoteInEscaped.call(me, character); break;
+            case STATE_NON_ESCAPED:
+                fromNonEscaped.call(me, character); break;
+        }
+
+        me.state.lastChar = character;
+    },
+
+    endRecord = function() {
+        var me = this;
+
+        if (me.record && me.record.started) {
+            endField.call(me);
+
+            if (me.config.onRecord) {
+                me.config.onRecord(me.record.fields);
+            }
+        }
+
+        me.record = {
+            started : false,
+            field : '',
+            fields : []
+        };
+
+        me.state.currentSymbol = STATE_RECORD;
+    },
+
+    fromRecord = function(character) {
+        var me = this;
+
+        switch (character) {
+            case CHAR_CR:
+                break;
+            case CHAR_LF:
+                if (!me.forceWinLinebreaks ||
+                        (me.forceWinLinebreaks && me.state.lastChar == CHAR_CR)
+                        ) {
+                    endRecord.call(me);
+                    break;
+                }
+            case CHAR_DQUOTE:
+                transitionTo.call(me, STATE_ESCAPED);
+                break;
+            case CHAR_COMMA:
+                endField.call(me);
+                break;
+            default:
+                transitionTo.call(me, STATE_NON_ESCAPED);
+                addChar.call(me, character);
+        }
+    },
+
+    fromEscaped = function(character) {
+        var me = this;
+
+        switch (character) {
+            case CHAR_DQUOTE:
+                transitionTo.call(me, STATE_DQUOTE_IN_ESCAPED);
+                return;
+            default:
+                addChar.call(me, character);
+        }
+    },
+
+    fromNonEscaped = function(character) {
+        var me = this;
+
+        switch (character) {
+            case CHAR_CR:
+                break;
+            case CHAR_LF:
+                if (!me.forceWinLinebreaks ||
+                        (me.forceWinLinebreaks && me.state.lastChar == CHAR_CR)
+                        ) {
+                    endRecord.call(me);
+                    break;
+                }
+            case CHAR_DQUOTE:
+                throw new Error("Unexpected symbol: \" in unescaped record");
+            case CHAR_COMMA:
+                endField.call(me);
+                break;
+            default:
+                addChar.call(me, character);
+        }
+    },
+
+    fromDquoteInEscaped = function(character) {
+        var me = this;
+
+        switch (character) {
+            case CHAR_DQUOTE:
+                transitionTo.call(me, STATE_ESCAPED);
+                addChar.call(me, character);
+                return;
+            default:
+                return fromNonEscaped.call(me, character);
+        }
+    },
+
+    transitionTo = function(symbol) {
+        this.state.currentSymbol = symbol;
+    },
+
+    addChar = function(character) {
+        var me = this;
+
+        me.record.started = true;
+        me.record.field += String.fromCharCode(character);
+    },
+
+    clearState = function() {
+        this.state = {};
+    },
+
+    endField = function() {
+        var me = this;
+
+        me.record.fields.push(me.record.field);
+        me.record.field = '';
+        me.state.currentSymbol = STATE_RECORD;
+    };
+
+    constructor.prototype = {
+
+        parse : function(string) {
+            var me = this;
+
+            for (var i = 0, len = string.length; i < len; i++) {
+                parseNext.call(me, string.charCodeAt(i));
+            }
+
+            endRecord.call(me);
+        }
+
+    };
+
+    return constructor;
+}();
